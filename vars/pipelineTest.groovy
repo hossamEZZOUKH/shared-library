36d212a195d7647443e4997e20569c273349403e
hossam ezzoukh <hossamzoukh@gmail.com>
2019-04-01 11:44:34 +0200

    src *

--- vars/pipelineTest.groovy
+++ vars/pipelineTest.groovy
@@ -7,7 +7,7 @@ def call(body) {
         body.resolveStrategy = Closure.DELEGATE_FIRST
         body.delegate = config
         body()*/
-        steps=new steps()
+       // steps=new steps()
 
 
         node {
